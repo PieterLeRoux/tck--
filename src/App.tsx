d1321a98ba9531a622f79c0593cbde85ae4bbf00
leroux-p <pieter_le_roux@mckinsey.com>
2022-11-04 00:16:27 -0400

    Add Signup

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,23 @@
 import React from 'react'
 import { SafeAreaView, StyleSheet } from 'react-native'
 import { registerRootComponent } from 'expo'
-import Home from './screens/Home/Home'
+import SignUp from './screens/SignUp/SignUp'
 
 function App() {
   return (
     <SafeAreaView style={styles.container}>
-      <Home />
+      <SignUp />
     </SafeAreaView>
   )
 }
 
 const styles = StyleSheet.create({
-  container: {}
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
 })
 
 export default registerRootComponent(App)
--- src/screens/SignUp/SignUp.tsx
+++ src/screens/SignUp/SignUp.tsx
@@ -1,19 +1,38 @@
-import React from 'react'
-import { ScrollView, StyleSheet, Text } from 'react-native'
+import React, { useState } from 'react'
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native'
 
 export default function SignUp() {
+  const [name, onChangeName] = useState('')
+  const [date, onChangeDate] = useState('')
+
   return (
-    <ScrollView style={styles.container}>
-      <Text>Sign up</Text>
-    </ScrollView>
+    <View style={styles.container}>
+      <Text style={styles.heading}>Welcome to tck--</Text>
+      <Text style={styles.body}>Please enter the following details to get started:</Text>
+      <TextInput style={styles.input} onChangeText={onChangeName} value={name} placeholder="Name" />
+      <TextInput style={styles.input} onChangeText={onChangeDate} value={date} placeholder="Date" />
+      <Button title="Submit" onPress={() => {}} />
+    </View>
   )
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center'
+    alignItems: 'center'
+  },
+  heading: {
+    fontSize: 32,
+    fontWeight: 'bold'
+  },
+  body: {
+    fontSize: 16
+  },
+  input: {
+    height: 50,
+    width: 300,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10
   }
 })
